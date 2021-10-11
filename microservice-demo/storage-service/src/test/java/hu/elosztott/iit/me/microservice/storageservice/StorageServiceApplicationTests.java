a236631db031e115107a371eddd31f55f5be81f4
Mádi Viktor <madi.viktor@tigra.hu>
2021-10-11 10:45:50 +0200

    ea:1004 - Eureka microservices

---
+++ microservice-demo/storage-service/src/test/java/hu/elosztott/iit/me/microservice/storageservice/StorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.elosztott.iit.me.microservice.storageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
